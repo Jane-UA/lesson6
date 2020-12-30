78bd9f25b7addb01c5de0731650cb3c1d4c1a648
Jane <73879019+Jane-UA@users.noreply.github.com>
2020-12-30 11:03:04 +0200

    task1

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def make_country(country_name, capital):
+    return {'country': country_name, 'capital': capital}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_values(any_dict):
+    print(any_dict.values())
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    d = make_country('Ukraine', 'Kiev')
+    print_values(d)
